7da768f4a3a7dd2f85135a8f84f8ca51137fc9c1
santjc <sjcolombatto@hotmail.com>
2023-03-24 17:56:36 -0300

    [feature]: new 3d models - camera work - socials html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Html, Stage, useGLTF } from '@react-three/drei';
+import { Center, Html, OrbitControls, Stage, useGLTF } from '@react-three/drei';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import gsap, { Power1, Power2 } from 'gsap';
 import Head from 'next/head';
@@ -8,6 +8,8 @@ import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { degToRad, lerp } from 'three/src/math/MathUtils';
 
 import Arrow from '@components/Icons/Arrow';
+import GithubIcon from '@components/Icons/Github';
+import LinkedInIcon from '@components/Icons/LinkedinIcon';
 import SuspenseFallback from '@components/SuspenseFallback';
 
 import styles from '@styles/Home.module.scss';
@@ -22,12 +24,13 @@ export default function Home() {
         <Canvas
           style={{ width: '100vw', height: '100vh' }}
           flat
-          camera={{ position: [0, 35, 100], fov: 60 }}
+          camera={{ position: [0, 15, 100], fov: 60 }}
           shadows
         >
           <color attach="background" args={['#f5f3e6']} />
 
           <Stage intensity={0.7} adjustCamera={false} shadows="contact">
+            <OrbitControls />
             <Model />
           </Stage>
         </Canvas>
@@ -54,33 +57,18 @@ function Model(props: JSX.IntrinsicElements['group']) {
   const modelRef = useRef<Group>(null!);
   const tl = useRef<GSAPTimeline>();
   const htmlTl = useRef<GSAPTimeline>();
-  const htmlRef = useRef<HTMLDivElement>(null);
+  const htmlRef = useRef<Group>(null);
   const playBtnRef = useRef<HTMLButtonElement>(null);
   const reverseBtnRef = useRef<HTMLButtonElement>(null);
   const screenModelRef = useRef<Mesh>(null!);
   const landingTextRef = useRef<HTMLDivElement>(null);
-  const modelScreenHtml = useRef<HTMLDivElement>(null);
-  const { viewport } = useThree();
-  const [isMobile, setIsMobile] = useState<boolean>(
-    viewport.width * viewport.factor < 768
-  );
-  const [modelDistance, setModelDistance] = useState<any>({
-    x: 0,
-    y: !isMobile ? 35 : 25,
-    z: !isMobile ? 100 : 75,
-  });
-  useLayoutEffect(() => {
-    setModelDistance({
-      x: 0,
-      y: !isMobile ? 50 : 25,
-      z: !isMobile ? 100 : 75,
-    });
-  }, [viewport.width]);
+  const socialHtmlRef = useRef<HTMLDivElement>(null);
+  const { viewport, camera } = useThree();
 
   useEffect(() => {
     tl.current = gsap.timeline({ paused: true });
     htmlTl.current = gsap.timeline({ paused: true });
-    modelRef.current?.position.set(0, 1, 0);
+    modelRef.current?.position.set(0, 5, 0);
     modelRef.current?.rotation.set(0, 0, 0);
     screenModelRef.current?.rotation.set(degToRad(270), 0, Math.PI);
 
@@ -91,22 +79,19 @@ function Model(props: JSX.IntrinsicElements['group']) {
         ease: Power2.easeInOut,
         delay: 0.5,
       })
-      .to(
-        modelRef.current?.position,
-        {
-          duration: 1,
-          y: lerp(0, modelDistance.y, 0.7),
-          z: lerp(modelRef.current?.position.z, modelDistance.z, 0.75),
-          ease: Power1.easeInOut,
-        },
-        'movement'
-      )
       .to(modelRef.current?.rotation, {
-        duration: 0.5,
-        y: lerp(modelRef.current?.rotation.y, 8.98, 0.7),
-        x: lerp(modelRef.current?.rotation.x, -0.7, 0.7),
+        duration: 0.7,
+        y: Math.PI * 2,
+        x: 0.1,
+        ease: Power2.easeInOut,
+      })
+      .to(camera.position, {
+        duration: 0.7,
+        z: 50,
         ease: Power1.easeInOut,
       });
+
+    camera.lookAt(modelRef.current?.position);
   }, []);
 
   const handlePlay = () => {
@@ -116,6 +101,7 @@ function Model(props: JSX.IntrinsicElements['group']) {
           duration: 0.5,
           autoAlpha: 0,
           css: {
+            display: 'none',
             opacity: 0,
           },
           ease: Power2.easeInOut,
@@ -141,6 +127,15 @@ function Model(props: JSX.IntrinsicElements['group']) {
             pointerEvents: 'all',
           },
           duration: 0.5,
+        })
+        .to(socialHtmlRef.current, {
+          css: {
+            display: 'flex',
+            opacity: 1,
+          },
+          autoAlpha: 1,
+          duration: 0.5,
+          ease: Power2.easeInOut,
         });
     }
     tl.current?.play();
@@ -155,9 +150,9 @@ function Model(props: JSX.IntrinsicElements['group']) {
   //TODO: NO SCROLL - BUTTONS - ICONS INSIDE PC SCREEN - COPY  https://codesandbox.io/s/mixing-html-and-webgl-w-occlusion-9keg6
   return (
     <group>
-      <group>
-        <Html fullscreen position={[0, 10, 0]}>
-          <div ref={htmlRef} className={styles.modelControls}>
+      <group ref={htmlRef}>
+        <Html fullscreen position={[0, 0, -20]}>
+          <div className={styles.modelControls}>
             <button
               ref={reverseBtnRef}
               className={styles.reverseBtn}
@@ -168,6 +163,25 @@ function Model(props: JSX.IntrinsicElements['group']) {
             <div ref={landingTextRef} className={styles.landingText}>
               <h1>Hi! Im Santiago</h1>
               <h2>Web Developer from Argentina</h2>
+            </div>
+            <div
+              ref={socialHtmlRef}
+              className={`${styles.landingText} ${styles.socials}`}
+            >
+              <a
+                href={'https://www.github.com/santjc'}
+                rel={'noreferrer'}
+                target={'_blank'}
+              >
+                <GithubIcon />
+              </a>
+              <a
+                href={'https://www.linkedin.com/in/santco'}
+                rel={'noreferrer'}
+                target={'_blank'}
+              >
+                <LinkedInIcon />
+              </a>
             </div>
             <button
               ref={playBtnRef}
@@ -196,26 +210,10 @@ function Model(props: JSX.IntrinsicElements['group']) {
           position={[0, 0, -10.3]}
           rotation={[Math.PI, 0, Math.PI]}
           scale={[100, 100, 88.24]}
-        >
-          <Html
-            position={[0, 0, 0]}
-            rotation={[-Math.PI, 0, 0]}
-            scale={0.1}
-            occlude
-          >
-            <div
-              className={styles.modelScreenContainer}
-              onPointerDown={(e) => e.stopPropagation()}
-            ></div>
-          </Html>
-        </mesh>
-        <mesh
-          position={[0, 0, 0]}
-          rotation={[-Math.PI / 2, 0, 0]}
-          receiveShadow
-        >
-          <planeGeometry attach="geometry" args={[300, 300]} />
-          <shadowMaterial attach="material" color="#171717" opacity={0.7} />
+        />
+        <mesh castShadow receiveShadow position={[0, -3.5, 0]}>
+          <cylinderGeometry args={[25, 25, 5, 64]} />
+          <meshPhysicalMaterial roughness={0.2} color={'#000'} />
         </mesh>
       </group>
     </group>

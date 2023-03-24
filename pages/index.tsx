3a4f3eef9f2daf898a408d9bcf1b4575b6218abc
santjc <sjcolombatto@hotmail.com>
2023-03-24 14:37:08 -0300

    [fix]: animations - html on screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
-import {
-  Center,
-  Environment,
-  Html,
-  OrbitControls,
-  ScrollControls,
-  Stage,
-  useGLTF,
-  useScroll,
-} from '@react-three/drei';
+import { Html, Stage, useGLTF } from '@react-three/drei';
 import { Canvas, useFrame } from '@react-three/fiber';
 import gsap, { Power1, Power2 } from 'gsap';
 import Head from 'next/head';
-import Image from 'next/image';
-import { Suspense, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { Suspense, useEffect, useRef } from 'react';
 import { Group, Mesh } from 'three';
 import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { degToRad, lerp } from 'three/src/math/MathUtils';
 
 import Arrow from '@components/Icons/Arrow';
-import Overlay from '@components/Overlay';
 import SuspenseFallback from '@components/SuspenseFallback';
 
 import styles from '@styles/Home.module.scss';
@@ -47,15 +36,6 @@ export default function Home() {
   );
 }
 
-function Plane() {
-  return (
-    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[300, 300]} />
-      <shadowMaterial attach="material" color="#171717" opacity={0.7} />
-    </mesh>
-  );
-}
-
 type GLTFResult = GLTF & {
   nodes: {
     Frame_ComputerFrame_0: THREE.Mesh;
@@ -73,74 +53,83 @@ function Model(props: JSX.IntrinsicElements['group']) {
   ) as GLTFResult;
   const modelRef = useRef<Group>(null!);
   const tl = useRef<GSAPTimeline>();
+  const htmlTl = useRef<GSAPTimeline>();
   const htmlRef = useRef<HTMLDivElement>(null);
   const playBtnRef = useRef<HTMLButtonElement>(null);
   const reverseBtnRef = useRef<HTMLButtonElement>(null);
   const screenModelRef = useRef<Mesh>(null!);
   const landingTextRef = useRef<HTMLDivElement>(null);
+  const modelScreenHtml = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     tl.current = gsap.timeline({ paused: true });
+    htmlTl.current = gsap.timeline({ paused: true });
+    modelRef.current?.position.set(0, 1, 0);
+    modelRef.current?.rotation.set(0, 0, 0);
+    screenModelRef.current?.rotation.set(degToRad(270), 0, Math.PI);
 
-    if (reverseBtnRef.current) {
-      gsap.set(reverseBtnRef.current, {
-        autoAlpha: 0,
-        css: {
-          opacity: 0,
-        },
-      });
-    }
-    if (modelRef.current) {
-      modelRef.current?.position.set(0, 1, 0);
-      modelRef.current?.rotation.set(0, 0, 0);
-    }
-    if (screenModelRef.current) {
-      screenModelRef.current?.rotation.set(degToRad(270), 0, Math.PI);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (tl.current) {
-      tl.current
-        ?.to(playBtnRef.current, {
-          duration: 0.5,
-          autoAlpha: 0,
-          css: {
-            opacity: 0,
-          },
-        })
-        .to(landingTextRef.current, {
-          duration: 0.5,
-          autoAlpha: 0,
-          css: {
-            opacity: 0,
-          },
-          ease: Power2.easeInOut,
-        })
-
-        .to(screenModelRef.current?.rotation, {
-          duration: 0.5,
-          x: Math.PI,
-          ease: Power2.easeInOut,
-          delay: 1,
-        })
-        .to(modelRef.current?.position, {
+    tl.current
+      ?.to(screenModelRef.current?.rotation, {
+        duration: 0.5,
+        x: Math.PI,
+        ease: Power2.easeInOut,
+        delay: 0.5,
+      })
+      .to(
+        modelRef.current?.position,
+        {
           duration: 1,
           y: lerp(0, 50, 0.7),
           z: lerp(modelRef.current?.position.z, 125, 0.75),
           ease: Power1.easeInOut,
-        })
-        .to(modelRef.current?.rotation, {
+        },
+        'movement'
+      )
+      .to(modelRef.current?.rotation, {
+        duration: 0.5,
+        y: lerp(modelRef.current?.rotation.y, 8.98, 0.7),
+        x: lerp(modelRef.current?.rotation.x, -0.7, 0.7),
+        ease: Power1.easeInOut,
+      });
+  }, []);
+
+  const handlePlay = () => {
+    if (reverseBtnRef && playBtnRef && landingTextRef) {
+      htmlTl.current
+        ?.to(landingTextRef && landingTextRef.current, {
           duration: 0.5,
-          y: lerp(modelRef.current?.rotation.y, 8.98, 0.7),
-          x: lerp(modelRef.current?.rotation.x, -0.7, 0.7),
-          ease: Power1.easeInOut,
+          autoAlpha: 0,
+          css: {
+            opacity: 0,
+          },
+          ease: Power2.easeInOut,
+        })
+        .to(reverseBtnRef.current, {
+          autoAlpha: 0,
+          css: {
+            opacity: 0,
+          },
+        })
+        .to(playBtnRef && playBtnRef.current, {
+          duration: 0.5,
+          autoAlpha: 0,
+          css: {
+            opacity: 0,
+          },
         })
         .to(reverseBtnRef.current, {
           autoAlpha: 1,
           duration: 0.5,
         });
     }
-  }, [tl]);
+    tl.current?.play();
+    htmlTl.current?.play();
+  };
+
+  const handleReverse = () => {
+    tl.current?.reverse();
+    htmlTl.current?.reverse();
+  };
 
   //TODO: NO SCROLL - BUTTONS - ICONS INSIDE PC SCREEN - COPY  https://codesandbox.io/s/mixing-html-and-webgl-w-occlusion-9keg6
   return (
@@ -151,7 +140,7 @@ function Model(props: JSX.IntrinsicElements['group']) {
             <button
               ref={reverseBtnRef}
               className={styles.reverseBtn}
-              onClick={() => tl.current?.reverse()}
+              onClick={handleReverse}
             >
               <Arrow size={'22px'} />
             </button>
@@ -162,15 +151,10 @@ function Model(props: JSX.IntrinsicElements['group']) {
             <button
               ref={playBtnRef}
               className={styles.playBtn}
-              onClick={() => tl.current?.play()}
+              onClick={handlePlay}
             >
               <Arrow size={'22px'} />
             </button>
-          </div>
-          <div className={styles.computerScreen}>
-            <div className={styles.screenContent}>
-              <h1 className={styles.headline}>Hi, I&amp;m Santco</h1>
-            </div>
           </div>
         </Html>
       </group>
@@ -191,8 +175,27 @@ function Model(props: JSX.IntrinsicElements['group']) {
           position={[0, 0, -10.3]}
           rotation={[Math.PI, 0, Math.PI]}
           scale={[100, 100, 88.24]}
-        ></mesh>
-        <Plane />
+        >
+          <Html
+            position={[0, 0, 0]}
+            rotation={[-Math.PI, 0, 0]}
+            scale={0.1}
+            occlude
+          >
+            <div
+              className={styles.modelScreenContainer}
+              onPointerDown={(e) => e.stopPropagation()}
+            ></div>
+          </Html>
+        </mesh>
+        <mesh
+          position={[0, 0, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          receiveShadow
+        >
+          <planeGeometry attach="geometry" args={[300, 300]} />
+          <shadowMaterial attach="material" color="#171717" opacity={0.7} />
+        </mesh>
       </group>
     </group>
   );
